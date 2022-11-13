6b53f492afa0efcddbaa56d7b3411a92654d9b9e
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-11-13 23:48:05 +0200

    upload mapping

--- course solidity/mapping.sol
+++ course solidity/mapping.sol
@@ -1,1 +1,22 @@
 //Hello
+// SPDX-License-Indentifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage{
+
+    mapping(string => uint256) public findPerson;
+
+    struct People{
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    People[] public people;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({name: _name, favoriteNumber: _favoriteNumber});
+        people.push(newPerson);
+        findPerson[_name] = _favoriteNumber;
+    
+    }
+}
