f057b10f8317c94ed910f321c3313a08ae67b965
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-11-13 23:34:24 +0200

    object code

--- course solidity/objecto.sol
+++ course solidity/objecto.sol
@@ -1,1 +1,21 @@
 //Hello
+// SPDX-License-Indentifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage{
+
+    
+
+    struct People{
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    People[] public people;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({name: _name, favoriteNumber: _favoriteNumber});
+        people.push(newPerson);
+    
+    }
+}
